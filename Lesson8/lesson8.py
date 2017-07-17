005cb67f031075e3cfcb1f779c169d665a687ce4
Ameet Rahane <ameetrahane@gmail.com>
2017-07-17 22:03:01 +0100

    added one plot

---
+++ Lesson8/lesson8.py
@@ -0,0 +1,15 @@
+import matplotlib
+matplotlib.use('Agg')
+from datascience import *
+import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('fivethirtyeight')
+
+
+
+
+def positive_correlation():
+    disease_data=(Table.read_table('prisoners_over_time.csv'))
+    disease_data.sort('Prisoners admitted to Hospitals').plot('Prisoners admitted to Hospitals')
+    print(disease_data.scatter('Prisoners admitted to Hospitals'))
+    
