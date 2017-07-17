766610e5cd07200ee55b89aea1e4e0eddf1d69a2
Ameet Rahane <ameetrahane@gmail.com>
2017-07-17 22:57:44 +0100

    added correlation coefficient for lesson 8

--- Lesson8/lesson8.py
+++ Lesson8/lesson8.py
@@ -11,5 +11,5 @@ plt.style.use('fivethirtyeight')
 def positive_correlation():
     disease_data=(Table.read_table('prisoners_over_time.csv'))
     disease_data.sort('Prisoners admitted to Hospitals').plot('Prisoners admitted to Hospitals')
-    print(disease_data.scatter('Prisoners admitted to Hospitals'))
+    disease_data.scatter('Prisoners admitted to Hospitals')
     
